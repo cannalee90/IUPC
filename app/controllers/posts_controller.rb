96166e5c147588dfe885c5717d2072328724dffd
kangho <cannalee90@gmail.com>
2015-11-04 19:15:42 +0900

    modified implement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
+      edited = Post.new(post_params)
+      if @post.authenticate(edited.password) and @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
