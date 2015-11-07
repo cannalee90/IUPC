14ff2113c4e7c86bc6cbd588353d8ac7c43d2fc7
이강호 (aka. hakgb11) <cannalee90@gmail.com>
2015-11-07 06:34:00 +0000

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151031061650) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "name"
+    t.string   "email"
+    t.string   "password"
     t.string   "password_digest"
   end
 
