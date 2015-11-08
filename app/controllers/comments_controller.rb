7b0687f38400b9beac053cbca1554c206eeb88b8
이강호 (aka. hakgb11) <cannalee90@gmail.com>
2015-11-08 10:08:21 +0000

    delete modify

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to :back }
 
     end
   end
