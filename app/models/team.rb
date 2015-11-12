f965376a00b4ff3b9cf206896dd190e372832cfb
이강호 (aka. hakgb11) <cannalee90@gmail.com>
2015-11-12 18:55:43 +0000

    regex change

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,8 @@ class Team < ActiveRecord::Base
   has_many :participants
   accepts_nested_attributes_for :participants
   validates_associated :participants
-  VALID_NAME_REGEX = /[\w\-_\s]+/
-  validates :name,  presence: true, 
+  VALID_NAME_REGEX = /\A[a-zA-Z0-9][a-zA-Z0-9\s]*[a-zA-Z0-9]\Z/
+  validates :name,  presence: true,   
                     length: { maximum: 30 },
                     format: { with: VALID_NAME_REGEX }, 
                     uniqueness: true
