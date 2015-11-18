f3827353ecaacc8a3034306d3510efd6d1757d0d
이강호 (aka. hakgb11) <cannalee90@gmail.com>
2015-11-18 13:48:16 +0000

    number of checked list update

--- app/assets/javascripts/teamlist.js
+++ app/assets/javascripts/teamlist.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     $("#checkAll").change(function () {
         $("input:checkbox").prop('checked', $(this).prop("checked"));
     });
+    $("input").change(function () {
+        $("span#checkedReceiver").text($("input:checked").size());
+    });
+    
 });
 
