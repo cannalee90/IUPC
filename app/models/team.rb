e0f8fc8ee6edbd764e56ffcdeeac47df2fcdd8a4
cannalee90 <cannalee90@gmail.com>
2016-04-24 20:29:09 +0900

    schema bug fix

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,5 @@
 class Team < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :participants, dependent: :destroy
+  has_many :participants
   accepts_nested_attributes_for :participants
   VALID_NAME_REGEX = /\A[a-zA-Z0-9][a-zA-Z0-9\s]*[a-zA-Z0-9]\Z/
   validates :name,  presence: true,
